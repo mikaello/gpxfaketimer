9aec40ccd513764721fc8261998cb0b05b5e7178
mikaello <2505178+mikaello@users.noreply.github.com>
2020-05-03 16:44:18 +0200

    feat: function to evenly distribute timestamps among track points

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,5 +1,106 @@
-describe("test", () => {
-  test("test", () => {
-    expect(true).toBeTruthy();
+import { createTimestampsEvenly, getDistributedTimeStamps } from "../src/index";
+
+const startExampleTime = 1588100000000;
+const endExampleTime = 1588490300000;
+
+// <?xml version="1.0" encoding="UTF-8"?>
+const createGpxWithOneTrackPoint = (time: string = "") => {
+  return `
+<?xml version="1.0" encoding="UTF-8"?>
+<gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.0">
+  <name>Example gpx</name>
+  <trk><name>Example gpx</name><number>1</number><trkseg>
+    <trkpt lat="46.57608333" lon="8.89241667"><ele>2376</ele>${time}</trkpt>
+  </trkseg></trk>
+</gpx>`.trim();
+};
+
+const createGpxWithFiveTrackPoints = (
+  time1: string = "",
+  time2: string = "",
+  time3: string = "",
+  time4: string = "",
+  time5: string = "",
+) => {
+  return `
+<?xml version="1.0" encoding="UTF-8"?>
+<gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.0">
+  <name>Example gpx</name>
+  <trk><name>Example gpx</name><number>1</number><trkseg>
+    <trkpt lat="46.57608333" lon="8.89241667"><ele>2376</ele>${time1}</trkpt>
+	  <trkpt lat="46.57619444" lon="8.89252778"><ele>2375</ele>${time2}</trkpt>
+	  <trkpt lat="46.57641667" lon="8.89266667"><ele>2372</ele>${time3}</trkpt>
+	  <trkpt lat="46.57650000" lon="8.89280556"><ele>2373</ele>${time4}</trkpt>
+    <trkpt lat="46.57638889" lon="8.89302778"><ele>2374</ele>${time5}</trkpt>
+  </trkseg></trk>
+</gpx>`.trim();
+};
+
+describe("evenly distributed timestamps", () => {
+  const gpxOneTrackPointWithTimestamp = createGpxWithOneTrackPoint(
+    `<time>${new Date(startExampleTime).toISOString()}</time>`,
+  );
+
+  test("file with only one point", () => {
+    expect(
+      createTimestampsEvenly(
+        createGpxWithOneTrackPoint(),
+        startExampleTime,
+        endExampleTime,
+      ),
+    ).toEqual(gpxOneTrackPointWithTimestamp);
+  });
+
+  test("file with five track points", () => {
+    const timeStamps = getDistributedTimeStamps(
+      5,
+      startExampleTime,
+      endExampleTime,
+    );
+    const gpxFiveTrackPointsWithTimestamp = createGpxWithFiveTrackPoints(
+      `<time>${new Date(timeStamps[0]).toISOString()}</time>`,
+      `<time>${new Date(timeStamps[1]).toISOString()}</time>`,
+      `<time>${new Date(timeStamps[2]).toISOString()}</time>`,
+      `<time>${new Date(timeStamps[3]).toISOString()}</time>`,
+      `<time>${new Date(timeStamps[4]).toISOString()}</time>`,
+    );
+
+    expect(
+      createTimestampsEvenly(
+        createGpxWithFiveTrackPoints(),
+        startExampleTime,
+        endExampleTime,
+      ),
+    ).toEqual(gpxFiveTrackPointsWithTimestamp);
+  });
+});
+
+describe("helper function for creating distributed timestamps", () => {
+  test("it returns correct for zero timestamps", () => {
+    expect(getDistributedTimeStamps(0, 10, 110)).toHaveLength(0);
+  });
+  test("it returns correct for one timestamp", () => {
+    expect(getDistributedTimeStamps(1, 10, 11)).toEqual([10]);
+  });
+
+  test("that it returns correct for start and end when multiple timestamps", () => {
+    const count = 5;
+    const startTime = 20;
+    const endTime = 100;
+    const timestamps = getDistributedTimeStamps(count, startTime, endTime);
+    expect(timestamps).toHaveLength(count);
+    expect(timestamps[0]).toEqual(startTime);
+    expect(timestamps[timestamps.length - 1]).toEqual(endTime);
+  });
+
+  test("it returns correct for multiple timestamps", () => {
+    const count = 5;
+    expect(getDistributedTimeStamps(count, 20, 100)).toEqual([
+      20,
+      40,
+      60,
+      80,
+      100,
+    ]);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,76 @@
-const a = 7;
+/**
+ * Annotate every track point in an GPX track with (evenly distributed) timestamps.
+ * @param gpxContent GPX content (XML string)
+ * @param startTime start of GPX track (milliseconds since epoch)
+ * @param endTime end of GPX track (milliseconds since epoch)
+ */
+export const createTimestampsEvenly = (
+  gpxContent: string,
+  startTime: number,
+  endTime: number,
+) => {
+  const parser = new DOMParser();
+  const gpxDoc = parser.parseFromString(gpxContent, "text/xml");
+
+  if (gpxDoc.documentElement.nodeName === "parsererror") {
+    throw new Error("Could not parse GPX content");
+  }
+
+  const track = gpxDoc.documentElement.querySelector("trk");
+  const trackSegment = track?.querySelector("trkseg");
+
+  if (trackSegment == null) {
+    return gpxContent;
+  }
+
+  const trackPoints = trackSegment.querySelectorAll("trkpt");
+  const timeStamps = getDistributedTimeStamps(
+    trackPoints.length,
+    startTime,
+    endTime,
+  );
+
+  trackPoints.forEach((point, index) => {
+    const time = new Date(timeStamps[index]).toISOString();
+    point.insertAdjacentHTML("beforeend", `<time>${time}</time>`);
+  });
+
+  const serializer = new XMLSerializer();
+
+  return `<?xml version="1.0" encoding="${gpxDoc.inputEncoding}"?>\n` +
+    serializer.serializeToString(gpxDoc);
+};
+
+export const getDistributedTimeStamps = (
+  count: number,
+  startTime: number,
+  endTime: number,
+) => {
+  if (endTime < startTime) {
+    console.error(
+      `end time (${endTime}) is smaller than start time (${startTime})`,
+    );
+    return [];
+  }
+
+  if (count === 0) {
+    return [];
+  } else if (count === 1) {
+    return [startTime];
+  }
+
+  const milliSecondDiff = endTime - startTime;
+
+  const timeStep = Math.floor(milliSecondDiff / (count - 1));
+
+  const timeStamps = Array(count)
+    .fill(0)
+    .map((_, index) => startTime + index * timeStep);
+
+  if (count > 1) {
+    // Last time stamp must be end time
+    timeStamps[timeStamps.length - 1] = endTime;
+  }
+
+  return timeStamps;
+};
