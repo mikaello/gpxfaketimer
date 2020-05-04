fc6638a6a5e3bcf3ed7f71a37448236b1efc2803
mikaello <2505178+mikaello@users.noreply.github.com>
2020-05-04 22:15:08 +0200

    feat: enhance example code

--- example/helper.ts
+++ example/helper.ts
@@ -1,5 +1,4 @@
 import { createTimestampsEvenly } from "@mikaello/gpxfaketimer";
-import { time } from "console";
 
 declare global {
   interface Window {
@@ -11,16 +10,28 @@ export const generateTimestamps = () => {
   const timestampButton = document.getElementById("generate-timestamps-btn");
 
   timestampButton.onclick = () => {
-    const gpx = document.getElementById("gpx-no-timestamp").innerText;
+    const gpx = (<HTMLTextAreaElement>(
+      document.getElementById("gpx-no-timestamp")
+    )).value;
 
     const now = new Date().valueOf();
-    console.log("gpx", gpx);
-    const timeGpx = createTimestampsEvenly(gpx, now, now + 60 * 60 * 1000);
 
-    console.log("timegpx", timeGpx);
+    const gpxPaste = <HTMLTextAreaElement>(
+      document.getElementById("gpx-with-timestamp")
+    );
 
-    const gpxPaste = document.getElementById("gpx-with-timestamp");
-    gpxPaste.innerText = timeGpx;
+    try {
+      const timeGpx = createTimestampsEvenly(gpx, now, now + 60 * 60 * 1000);
+      gpxPaste.value = timeGpx;
+    } catch (e) {
+      const errorNode = document.createElement("p");
+      errorNode.textContent = "ERROR PARSING";
+      errorNode.style.fontWeight = "700";
+      errorNode.style.color = "red";
+
+      document.insertBefore(errorNode, gpxPaste);
+      gpxPaste.value = e;
+    }
     gpxPaste.style.visibility = "visible";
   };
 };
